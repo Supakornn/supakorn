d1a706802d259231f23149c54c9920bdca10df03
Supakornn <supakorn0728@gmail.com>
2025-03-16 00:57:26 +0700

    :bug: add link to github in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,11 @@ export function Footer() {
     return (
         <footer className="py-6 mt-auto">
             <div className="text-center text-sm text-muted-foreground">
-                © 2025 <span className="text-yellow-500">Supakornn</span>. All rights reserved.
+                © 2025{" "}
+                <a href="https://github.com/supakornn">
+                    <span className="text-yellow-500">Supakornn</span>
+                </a>
+                . All rights reserved.
             </div>
         </footer>
     );
