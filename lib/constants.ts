a4a3c316eef99f2101b6e0563dd93a52e3400bc1
Supakornn <supakorn0728@gmail.com>
2025-03-22 14:38:55 +0700

    feat(achievements): change from certificate to achivements

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 export const NAVIGATION_ITEMS = [
     { title: "About", href: "/about" },
     { title: "Projects", href: "/projects" },
-    { title: "Certificates", href: "/certificates" }
+    { title: "Achievements", href: "/achievements" }
 ] as const;
 
 export const SITE_CONFIG = {
