303add776a2f57fc0b89c4f5f4fe2a9ede2926cd
Supakornn <78318848+Supakornn@users.noreply.github.com>
2025-05-19 14:27:38 +0700

    fix: postiion of webring logo

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 export function Footer() {
   return (
     <footer className="relative mt-auto py-6">
-      <div className="container mx-auto px-6">
-        <div className="relative">
-          <div className="text-center text-sm text-muted-foreground">
+      <div className="w-full px-6">
+        <div className="relative flex items-center">
+          <div className="flex-1 text-center text-sm text-muted-foreground">
             © {new Date().getFullYear()}{' '}
             <a href={SITE_CONFIG.github}>
               <span className="text-yellow-500">{SITE_CONFIG.name}</span>
@@ -16,7 +16,7 @@ export function Footer() {
           <a
             href="https://webring.wonderful.software#supakorn.info"
             title="วงแหวนเว็บ"
-            className="absolute right-0 top-1/2 -translate-y-1/2"
+            className="absolute right-6"
           >
             <div className="group relative h-8 w-8 opacity-60 hover:text-yellow-700 hover:opacity-100">
               <Image
@@ -24,7 +24,7 @@ export function Footer() {
                 width={32}
                 height={32}
                 src="https://webring.wonderful.software/webring.black.svg"
-                className="hover:rotate-360 transition-transform duration-700 group-hover:[filter:invert(48%)_sepia(13%)_saturate(3207%)_hue-rotate(0deg)_brightness(95%)_contrast(95%)]"
+                className="transition-transform duration-700 hover:rotate-360 group-hover:[filter:invert(48%)_sepia(13%)_saturate(3207%)_hue-rotate(0deg)_brightness(95%)_contrast(95%)]"
               />
             </div>
           </a>
