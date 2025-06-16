d1986baa6ee96fdc4560305ba37e829027765e5d
Supakornn <78318848+Supakornn@users.noreply.github.com>
2025-06-16 11:38:38 +0700

    remove: delete cobol101 writting

--- app/writings/data.ts
+++ app/writings/data.ts
@@ -2,18 +2,10 @@ import { BlogPost } from '@/types';
 
 export const posts: BlogPost[] = [
   {
-    title: 'COBOL 101',
-    slug: 'cobol-101',
-    date: '2025-05-22',
-    tags: ['COBOL', 'Mainframe', 'Programming'],
-    description: 'Writing about COBOL and how to use it.',
-    link: '/writings/cobol-101',
-  },
-  {
     title: 'เขียนเว็บแบบไม่ง้อ JavaScript ด้วย HTMX',
     slug: 'htmx-basic',
     date: '2025-04-15',
-    tags: ['HTMX', 'Go', 'Web Development'],
+    tags: ['Thai', 'HTMX', 'Go', 'Web Development'],
     description: 'Writing about what is HTMX and how to use it to build a web application.',
     link: '/writings/htmx-basic',
   },
@@ -21,7 +13,7 @@ export const posts: BlogPost[] = [
     title: 'WebAssembly (WASM) ด้วยภาษา Zig — เขียนเว็บด้วยความเร็วระดับ Native',
     slug: 'wasm-with-zig',
     date: '2025-04-13',
-    tags: ['WebAssembly', 'Zig', 'Web Development'],
+    tags: ['Thai', 'WebAssembly', 'Zig', 'Web Development'],
     description: 'Writing about what is WebAssembly and how to use it with Zig.',
     link: '/writings/wasm-with-zig',
   },
@@ -29,7 +21,7 @@ export const posts: BlogPost[] = [
     title: 'STH-Mini-Web-CTF-2025 — Write Up',
     slug: 'sth-mini-web-ctf-2025-write-up',
     date: '2025-04-06',
-    tags: ['CTF', 'Hacking'],
+    tags: ['Thai', 'CTF', 'Hacking'],
     description: 'Write up for the STH-Mini-Web-CTF-2025.',
     link: '/writings/sth-mini-web-ctf-2025-write-up',
   },
